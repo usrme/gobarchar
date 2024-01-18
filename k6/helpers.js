80d86523863b23f51fd7eae539e39c4e0a1e3ded
Üllar Seerme <yates@protonmail.ch>
2024-01-18 20:33:18 +0200

    refactor(k6/helpers.js): don't export what isn't necessary

--- k6/helpers.js
+++ k6/helpers.js
@@ -1,7 +1,7 @@
 import { URLSearchParams } from "https://jslib.k6.io/url/1.0.0/index.js";
 import http from "k6/http";
 
-export const searchParams = new URLSearchParams([
+const searchParams = new URLSearchParams([
   ["December", "19"],
   ["February", "62"],
   ["March", "93"],
@@ -10,10 +10,10 @@ export const searchParams = new URLSearchParams([
   ["September", "70"],
 ]);
 
-export const duration = "10s";
-export const executor = "constant-vus";
-export const dRb = true;
-export const vus = 1;
+const duration = "10s";
+const executor = "constant-vus";
+const dRb = true;
+const vus = 1;
 
 export const getOptions = (name) => {
   return Object.assign(
